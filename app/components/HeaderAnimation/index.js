bfe5022c376a592a778746418e096753841e8b81
Ashish Shevale <shevaleashish@gmail.com>
2021-10-10 18:51:31 +0530

    Draw arcs in HeaderAnimation

--- app/components/HeaderAnimation/index.js
+++ app/components/HeaderAnimation/index.js
@@ -1,7 +1,20 @@
 import React from 'react'
 
+import Arc from 'components/Arc/Loadable'
+import useWindowSize from 'hooks/useWindowSize'
+import "./index.css"
+
 function HeaderAnimation() {
-  return <div />;
+  const [width, height] = useWindowSize ()
+
+  return (
+    <div id="HeaderAnimation__container">
+      <svg xmlns="http://www.w3.org/2000/svg" xlink="http://www.w3.org/1999/xlink" width={0.25*width} height={0.25*width}>
+
+        <Arc posX={0.125} posY={0.125} radius={0.1} startAngle={0} endAngle={90} viewport={width} />
+      </svg>
+    </div>
+  );
 }
 
 export default HeaderAnimation;
