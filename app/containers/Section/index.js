e8a68bb9338f7cd3382c5629b8e706a6858f5d3d
Ashish Shevale <shevaleashish@gmail.com>
2021-10-10 15:45:26 +0530

    Refactor Section container

---
+++ app/containers/Section/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container'
+
+const styles = {
+    color: "white",
+
+    backgroundImage: 'url(/section_background.jpg)',
+    backgroundSize: '100% 100%',
+    backgroundPosition: 'left top',
+
+    width: "75.1041vw",
+    height: "76.7592vh",
+
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+
+    borderRadius: "8px",
+    boxShadow: "5px 10px 12px 0px black"
+}
+
+export default function Section(props) {
+    return (
+        <Container style = {styles}>
+            {props.children}
+        </Container>
+    );
+}
