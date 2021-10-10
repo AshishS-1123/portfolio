ccd6a05d30c26fedb2d0850524d90b62c1b4e1cf
Ashish Shevale <shevaleashish@gmail.com>
2021-10-10 18:50:44 +0530

    Use window dimensions to draw arcs

--- app/components/Arc/index.js
+++ app/components/Arc/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { motion } from 'framer-motion'
 
-import { getPathData } from 'utils/geometry'
+import { getPathData, viewportToPixels } from 'utils/geometry'
 
-function Arc({ posX, posY, radius,  startAngle, endAngle }) {
+function Arc({ posX, posY, radius,  startAngle, endAngle, viewport }) {
 
   return (
     <motion.path
-      d={getPathData (posX, posY, radius, startAngle, endAngle)}
+      d={getPathData (posX * viewport, posX * viewport, radius * viewport, startAngle, endAngle)}
       fill="transparent"
       strokeWidth="12"
       stroke="red"
@@ -21,7 +21,8 @@ Arc.propTypes = {
   posY: PropTypes.number.isRequired,
   radius: PropTypes.number.isRequired,
   startAngle: PropTypes.number.isRequired,
-  endAngle: PropTypes.number.isRequired
+  endAngle: PropTypes.number.isRequired,
+  viewport: PropTypes.number.isRequired
 };
 
 export default Arc;
