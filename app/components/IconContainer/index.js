02c0e21c324f26ea1038282e8398679519a8d728
Ashish Shevale <shevaleashish@gmail.com>
2021-10-13 22:19:26 +0530

    Create basic component for displaying all icons

---
+++ app/components/IconContainer/index.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * IconContainer
+ *
+ */
+
+import React from 'react';
+// import PropTypes from 'prop-types';
+// import styled from 'styled-components';
+
+function IconContainer() {
+  return <div />;
+}
+
+IconContainer.propTypes = {};
+
+export default IconContainer;
