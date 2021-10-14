7b66b4d2ae93b1d158ae6eac721ea8b1ba765d9f
Ashish Shevale <shevaleashish@gmail.com>
2021-10-14 18:41:51 +0530

    Create basic NavArrows component

---
+++ app/components/NavArrows/index.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * NavArrows
+ *
+ */
+
+import React from 'react';
+// import PropTypes from 'prop-types';
+// import styled from 'styled-components';
+
+function NavArrows() {
+  return <div />;
+}
+
+NavArrows.propTypes = {};
+
+export default NavArrows;
