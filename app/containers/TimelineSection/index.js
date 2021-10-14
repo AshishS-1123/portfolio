de9f180ff1426409f8ac0797c2f12ec414750608
Ashish Shevale <shevaleashish@gmail.com>
2021-10-14 18:57:59 +0530

    Refactor for architecture

--- app/containers/TimelineSection/index.js
+++ app/containers/TimelineSection/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import TimelineComponent from 'components/TimelineComponent/Loadable'
+import TimelineComponent from 'containers/TimelineComponent/'
 import YearHolder from 'components/YearHolder/Loadable'
 import YearInfoHolder from 'components/YearInfoHolder/Loadable'
 import NavArrows from 'components/NavArrows/Loadable'
@@ -10,11 +10,7 @@ export function TimelineSection() {
   return (
     <div style={{fontSize: "25px", fontFamily: "Acme", textAlign: "center", paddingTop: "1.3vw"}}>
       Here Is A Snapshot Of My Journey Till Now
-      <TimelineComponent>
-        <YearHolder />
-        <YearInfoHolder />
-        <NavArrows />
-      </TimelineComponent>
+      <TimelineComponent />
     </div>
   );
 }
