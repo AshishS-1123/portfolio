40e91bc94cc2d5788b8c904be9990e1cfdb037c4
Ashish Shevale <shevaleashish@gmail.com>
2021-10-14 18:41:25 +0530

    Create basic Yearholder component

---
+++ app/components/YearHolder/index.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * YearHolder
+ *
+ */
+
+import React from 'react';
+// import PropTypes from 'prop-types';
+// import styled from 'styled-components';
+
+function YearHolder() {
+  return <div />;
+}
+
+YearHolder.propTypes = {};
+
+export default YearHolder;
