ac24b29ccfc6e1eceb7c9280c6b85450492d7a4a
Ashish Shevale <shevaleashish@gmail.com>
2021-10-14 18:41:37 +0530

    Create basic YearInfoHolder component

---
+++ app/components/YearInfoHolder/index.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * YearInfoHolder
+ *
+ */
+
+import React from 'react';
+// import PropTypes from 'prop-types';
+// import styled from 'styled-components';
+
+function YearInfoHolder() {
+  return <div />;
+}
+
+YearInfoHolder.propTypes = {};
+
+export default YearInfoHolder;
