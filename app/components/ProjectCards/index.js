b51de992d64cb596f4098b50ebccfcc4031dc70b
Ashish Shevale <shevaleashish@gmail.com>
2021-10-16 20:26:04 +0530

    Generate basic componen to display all projects

---
+++ app/components/ProjectCards/index.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * ProjectCards
+ *
+ */
+
+import React, { memo } from 'react';
+// import PropTypes from 'prop-types';
+// import styled from 'styled-components';
+
+function ProjectCards() {
+  return <div />;
+}
+
+ProjectCards.propTypes = {};
+
+export default memo(ProjectCards);
--- app/containers/ProjectSection/index.js
+++ app/containers/ProjectSection/index.js
@@ -1,11 +1,14 @@
 import React, { memo } from 'react'
 import PropTypes from 'prop-types'
 
+import ProjectCards from 'components/ProjectCards/Loadable'
+
 export function ProjectSection() {
   return (
     <div>
+      <ProjectCards />
     </div>
-  );
+  )
 }
 
 ProjectSection.propTypes = {
