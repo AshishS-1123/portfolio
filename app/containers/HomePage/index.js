caa3bf9abf928352130181c8b350521735663b70
Ashish Shevale <shevaleashish@gmail.com>
2021-10-16 20:19:48 +0530

    Create basic container for Project section

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -3,21 +3,23 @@ import React from 'react';
 import createSection from 'containers/Section'
 import HeroSection from 'containers/HeroSection'
 import TimelineSection from 'containers/TimelineSection'
+import ProjectSection from 'containers/ProjectSection'
 
 export default function HomePage() {
   const HeroSectionWrapped = createSection(HeroSection)
   const TimelineSectionWrapped = createSection(TimelineSection)
 
   const sections = [
-    createSection (HeroSection),
-    createSection (TimelineSection)
+    // createSection (HeroSection),
+    // createSection (TimelineSection),
+    createSection (ProjectSection)
   ]
 
   return (
     <React.Fragment>
       {
-        sections.map (Section => {
-          return <Section />
+        sections.map ((Section, idx) => {
+          return <Section key={idx}/>
         })
       }
     </React.Fragment>
---
+++ app/containers/ProjectSection/index.js
@@ -0,0 +1,14 @@
+import React, { memo } from 'react'
+import PropTypes from 'prop-types'
+
+export function ProjectSection() {
+  return (
+    <div>
+    </div>
+  );
+}
+
+ProjectSection.propTypes = {
+};
+
+export default memo (ProjectSection)
