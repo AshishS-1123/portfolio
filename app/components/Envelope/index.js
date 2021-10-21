880e5efe17d54eff9cd798facb008f3e8abe429f
Ashish Shevale <shevaleashish@gmail.com>
2021-10-21 18:32:39 +0530

    Create empty envelope container

---
+++ app/components/Envelope/index.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * Envelope
+ *
+ */
+
+import React from 'react';
+// import PropTypes from 'prop-types';
+// import styled from 'styled-components';
+
+function Envelope() {
+  return <div />;
+}
+
+Envelope.propTypes = {};
+
+export default Envelope;
--- app/containers/FooterSection/index.js
+++ app/containers/FooterSection/index.js
@@ -4,7 +4,8 @@ import PropTypes from 'prop-types'
 export function FooterSection() {
   return (
     <div>
-    footer</div>
+    footer
+    </div>
   )
 }
 
