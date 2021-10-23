8ebab642d708bf65a097bb3f906622a0ca6665d5
Ashish Shevale <shevaleashish@gmail.com>
2021-10-23 12:06:43 +0530

    Create empty Form component

---
+++ app/components/ContactForm/index.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * ContactForm
+ *
+ */
+
+import React from 'react';
+// import PropTypes from 'prop-types';
+// import styled from 'styled-components';
+
+function ContactForm() {
+  return <div />;
+}
+
+ContactForm.propTypes = {};
+
+export default ContactForm;
