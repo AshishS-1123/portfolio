bdc267376fd9702a02c8522f9808c4d9f62a8f5a
Ashish Shevale <shevaleashish@gmail.com>
2021-10-23 15:42:40 +0530

    Created basic Resume component

---
+++ app/components/Resume/index.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * Resume
+ *
+ */
+
+import React from 'react';
+// import PropTypes from 'prop-types';
+// import styled from 'styled-components';
+
+function Resume() {
+  return <div />;
+}
+
+Resume.propTypes = {};
+
+export default Resume;
