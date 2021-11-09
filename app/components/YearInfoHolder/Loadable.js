7480e56798a5eac3ceef38db5595de0032d2fac6
Ashish Shevale <shevaleashish@gmail.com>
2021-11-09 12:01:45 +0530

    Remove Timeline from portfolio

--- app/components/YearInfoHolder/Loadable.js
+++
@@ -1,9 +0,0 @@
-/**
- *
- * Asynchronously loads the component for YearInfoHolder
- *
- */
-
-import loadable from 'utils/loadable';
-
-export default loadable(() => import('./index'));
--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -3,28 +3,26 @@ import React from 'react';
 import createSection from 'containers/Section'
 
 import HeroSection from 'containers/HeroSection'
-import TimelineSection from 'containers/TimelineSection'
 import ProjectSection from 'containers/ProjectSection'
 import FooterSection from 'containers/FooterSection'
 import ResumeSection from 'containers/ResumeSection'
 
 export default function HomePage() {
 
-  const sections = [
-    createSection (HeroSection),
-    createSection (TimelineSection),
-    createSection (ProjectSection),
-    createSection (ResumeSection),
-    createSection (FooterSection)
-  ]
+    const sections = [
+        createSection(HeroSection),
+        createSection(ProjectSection),
+        createSection(ResumeSection),
+        createSection(FooterSection)
+    ]
 
-  return (
-    <React.Fragment>
-      {
-        sections.map ((Section, idx) => {
-          return <Section key={idx}/>
-        })
-      }
-    </React.Fragment>
-  );
+    return ( <
+        React.Fragment > {
+            sections.map((Section, idx) => {
+                return <Section key = { idx }
+                />
+            })
+        } <
+        /React.Fragment>
+    );
 }
