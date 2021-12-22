c7a7c785b4373432d3b2d0a770da808185f26c56
Ashish Shevale <shevaleashish@gmail.com>
2021-12-22 21:27:26 +0530

    Designed ContactBar component

--- app/components/ContactBar/index.js
+++ app/components/ContactBar/index.js
@@ -1,15 +1,27 @@
-/**
- *
- * ContactBar
- *
- */
-
 import React, { memo } from 'react';
-// import PropTypes from 'prop-types';
-// import styled from 'styled-components';
+import "./index.css";
 
 function ContactBar() {
-  return <div />;
+  const icon_details = [
+    { contact: "shevaleashish@gmail.com", icon: "/email.png" },
+    { contact: "linkedin.com/in/ashish-shevale", icon: "/linkedin.png" },
+    { contact: "github.com/AshishS-1123", icon: "/github.png" },
+    { contact: "+91-9022160734", icon: "/phone.png" },
+  ]
+
+  return (
+    <div className="ContactBar__container">
+      {
+        icon_details.map(item => {
+          return (
+            <div className="ContactBar__icon">
+              <img src={item.icon} />
+            </div>
+          )
+        })
+      }
+    </div>
+  )
 }
 
 ContactBar.propTypes = {};
