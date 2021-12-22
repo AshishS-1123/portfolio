3f5c1198322afb4bec8bc19045606fb90f98e1fa
Ashish Shevale <shevaleashish@gmail.com>
2021-12-22 20:48:41 +0530

    Create basic ContactBar component

---
+++ app/components/ContactBar/index.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * ContactBar
+ *
+ */
+
+import React, { memo } from 'react';
+// import PropTypes from 'prop-types';
+// import styled from 'styled-components';
+
+function ContactBar() {
+  return <div />;
+}
+
+ContactBar.propTypes = {};
+
+export default memo(ContactBar);
---
+++ app/components/ContactBar/tests/index.test.js
@@ -0,0 +1,37 @@
+/**
+ *
+ * Tests for ContactBar
+ *
+ * @see https://github.com/react-boilerplate/react-boilerplate/tree/master/docs/testing
+ *
+ */
+
+import React from 'react';
+import { render } from 'react-testing-library';
+// import 'jest-dom/extend-expect'; // add some helpful assertions
+
+import ContactBar from '../index';
+
+describe('<ContactBar />', () => {
+  it('Expect to not log errors in console', () => {
+    const spy = jest.spyOn(global.console, 'error');
+    render(<ContactBar />);
+    expect(spy).not.toHaveBeenCalled();
+  });
+
+  it('Expect to have additional unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+
+  /**
+   * Unskip this test to use it
+   *
+   * @see {@link https://jestjs.io/docs/en/api#testskipname-fn}
+   */
+  it.skip('Should render and match the snapshot', () => {
+    const {
+      container: { firstChild },
+    } = render(<ContactBar />);
+    expect(firstChild).toMatchSnapshot();
+  });
+});
