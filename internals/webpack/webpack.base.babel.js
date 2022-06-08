1b74444052447b2dd1117f2f4f0682f9eb7f25a4
Ashish Shevale <shevaleashish@gmail.com>
2022-06-08 14:43:37 +0530

    Set public path in webpack config

--- internals/webpack/webpack.base.babel.js
+++ internals/webpack/webpack.base.babel.js
@@ -12,7 +12,7 @@ module.exports = options => ({
     {
       // Compile into js/build.js
       path: path.resolve(process.cwd(), 'build'),
-      publicPath: '/',
+      publicPath: '/portfolio',
     },
     options.output,
   ), // Merge with env dependent settings
