5ca37efd9934512c714c6defc6b0494dc2132447
Ashish Shevale <shevaleashish@gmail.com>
2022-06-08 12:49:57 +0530

    Remove unwanted deps

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
 
 import HomePage from 'containers/HomePage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
@@ -9,10 +9,12 @@ import GlobalStyle from '../../global-styles';
 export default function App() {
   return (
     <div>
+      <Router>
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route component={NotFoundPage} />
       </Switch>
+      </Router>
       <GlobalStyle />
     </div>
   );
